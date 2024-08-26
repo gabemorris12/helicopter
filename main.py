feea4f488c5ebd839db778eed3024187831cb54c
Gabe Morris <gabemorris1231@gmail.com>
2024-08-26 15:41:01 -0500

    changed pot values

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ Kd = 500
 
 above_90 = (1500, 1500, 600)
 
-ang_points = (8400, 52550)  # angle at 0 degrees and 180 degrees
+ang_points = (10270, 54300)  # angle at 0 degrees and 180 degrees
 
 kill_button = Pin(17, Pin.IN, Pin.PULL_DOWN)
 poll = select.poll()
